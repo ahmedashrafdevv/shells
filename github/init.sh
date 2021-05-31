634b26779208dfd7a67efc7258900176e464cbb1
ahmed ashraf <ahmed.ashraf.devv@gmail.com>
2021-05-31 14:32:14 +0200

    init github config and install git

---
+++ github/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt install git
+git config --global user.name "ahmed ashraf"
+git config --global user.username "ahmedashrafdevv"
+git config --global user.email "ahmed.ashraf.devv@gmail.com"
+ssh -T git@github.com
