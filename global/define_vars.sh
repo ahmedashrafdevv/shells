df7a537e4b3c927242defb287cd5566adef09601
ahmed ashraf <ahmed.ashraf.devv@gmail.com>
2021-05-31 14:48:44 +0200

    export env variables shell added

---
+++ global/define_vars.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "export SHELLS_PATH=/home/ahmed/ahmedashrafdevv/global/shells/" >> ~/.profile
+echo "export GITHUB_TOKEN=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" >> ~/.profile
+
+
